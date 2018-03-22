9295818530f54ac88d113f0b9d7d30c45b9bb0ba
Raymond Berger <RayBB@users.noreply.github.com>
2018-03-22 17:14:52 -0400

    first register mockup

---
+++ frontend/housing/src/components/Register.vue
@@ -0,0 +1,45 @@
+<template>
+<div>
+    <h1>Register</h1>
+    <div id="firebaseui-auth-container">{{message}}</div>
+    <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/2.6.2/firebaseui.css" />
+</div>
+</template>
+
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Application"
+    };
+  },
+  props: ["vuer", "fbui"],
+  methods: {
+    testMethod: function() {
+      console.log("test method running");
+    }
+  },
+  created: function() {
+    let firebase = this.$props.vuer;
+    let firebaseui = require("firebaseui");
+
+    var uiConfig = {
+      signInSuccessUrl: "/",
+      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+      tosUrl: "<your-tos-url>",
+      signInFlow: "popup"
+    };
+
+    let ui;
+    if (firebaseui.auth.AuthUI.getInstance() == null) {
+      ui = new firebaseui.auth.AuthUI(firebase.auth());
+    } else {
+      ui = firebaseui.auth.AuthUI.getInstance();
+    }
+    ui.start("#firebaseui-auth-container", uiConfig);
+    //TODO: Check if there's an alternative to ui.start
+    
+  }
+};
+</script>
