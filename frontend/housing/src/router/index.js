98f2cd9000a2e64ca7cda9ad8a16d3b36017901f
apellis@eckerd.edu <ashleyellis@Ashleys-MacBook-Pro-2.local>
2018-03-23 17:14:14 -0400

    update router

--- frontend/housing/src/router/index.js
+++ frontend/housing/src/router/index.js
@@ -1,11 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 import Hello from '@/components/HelloWorld'
 import Squatting from '@/components/Squatting'
-=======
-import Foo from '@/components/Foo'
->>>>>>> c1a64c540483b4775e51f5af36f05dbfabed10dd
 import Traditional from '@/components/Traditional'
 import ViewApplications from '@/components/ViewApplications'
 import HousingRules from '@/components/HousingRules'
@@ -40,7 +36,6 @@ export default new Router({
             path: '/traditional',
             name: 'Traditional',
             component: Traditional
-<<<<<<< HEAD
         },
         {
             path: '/housingRules',
@@ -61,24 +56,6 @@ export default new Router({
             path: '/offcampus',
             name: 'OffCampus',
             component: OffCampus
-=======
-        },
-        {
-            path: '/housingRules',
-            name: 'HousingRules',
-            component: HousingRules
-        },
-        {
-            path: '/register',
-            name: 'Register',
-            component: Register,
-            props : true
-        },
-        {
-            path: '/roomSelector',
-            name: 'RoomSelector',
-            component: RoomSelector
->>>>>>> c1a64c540483b4775e51f5af36f05dbfabed10dd
         },
         {
             path: '/nu',
