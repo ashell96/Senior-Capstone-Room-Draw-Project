fd6b599595ce78577435dfb2e69defbd636252c8
apellis@eckerd.edu <ashleyellis@Ashleys-MacBook-Pro-2.local>
2018-03-25 22:44:32 -0400

    created ViewSubmissions component

---
+++ frontend/src/components/ViewSubmissions.vue
@@ -0,0 +1,62 @@
+<template>
+<span>
+    <h1 class="display-1">Application Choices</h1>
+    <div id="editor">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Submission Id:</th>
+                    <th scope="col">Primary Student E-mail:</th>
+                    <th scope="col">Submission Date:</th>
+                    <th scope="col">App ID:</th>
+                    <th scope="col">Room:</th>
+                    <th scope="col">Submission Status:</th>
+                </tr>
+            </thead>
+            <tbody>
+                
+                <tr v-for="item in submissions" :key="item.submission_id">
+                    <td id="sub_id">{{item.submission_id}}</td>
+                    <td id="stu_email">{{item.primary_student_email}}</td>
+                    <td id="sub_date">{{new Date(item.submission_date).toLocaleString()}}</td>
+                    <td id="app_id">{{item.app_id}}</td>
+                    <td id="room">{{item.room}}</td>
+                    <td id="sub_status">{{item.sub_status}}</td>
+
+                
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    </span>
+</template>
+
+<script>
+let axios = require("axios");
+module.exports = {
+  data: function() {
+    return {
+        submissions:"", 
+      // This is just default data for testing
+     
+    };
+  },
+  methods: {
+    
+    updateApps: function() {
+      let vm = this;
+      axios
+        .get("http://entropy7.nas.eckerd.edu:3000/submissions/")
+        .then(function(response) {
+          vm.$set(vm, "submissions", response.data);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  },
+  created: function() {
+    this.updateApps();
+  }
+};
+</script>
