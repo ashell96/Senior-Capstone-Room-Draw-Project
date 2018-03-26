1c3ce8857616858140389101651cd286014356ff
RayBB <rlberger@eckerd.edu>
2018-03-26 14:35:05 -0400

    update api

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,31 +15,28 @@ let connection = mysql.createConnection(configInfo.database);
 
 connection.connect(function (err) {
     if (err) {
-        //console.error('error connecting: ' + err.stack);
         throw ('error connecting: ' + err.stack);
     }
     console.log('connected as id ' + connection.threadId);
 });
-  
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
 
 // This is needed during testing to allow access when not serving webpages
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
-  
-function dbQuery(req,res,sql){
-    //res.send(sql);
-    //return;
+
+function dbQuery(req, res, sql) {
     console.log(sql);
     connection.query(sql, function (error, results, fields) {
         if (error) throw error;
-        console.log('The solution is: ', JSON.stringify(results) );
+        console.log('The solution is: ', JSON.stringify(results));
         res.send(results);
-      });
+    });
 }
 
 
@@ -88,22 +85,20 @@ app.get("/submissions/:submissionID" ,function(req, res){
 app.post("/submission", function(req,res){
     
     // Assuming traditional
-    let requester = req.body.requester;
-    let requestee = req.body.requestee;
-    let app_id = req.body.app_id; // we can eventually check submission type by app ID.
-    let room = req.body.room; // Alpha Beta 21
+    let requester = connection.escape(req.body.requester);
+    let requestee = connection.escape(req.body.requestee);
+    let app_id = connection.escape(req.body.app_id); // we can eventually check submission type by app ID.
+    let room = connection.escape(req.body.room); // Alpha Beta 21
     let status = "pending";
-
-    //if (requester == null || requestee == null || app_id == null || room == null)
-    /*console.log("Requester " + requester);
-    console.log("requestee " + requestee);
-    console.log("app_id " + app_id);
-    console.log("room " + room);
-    console.log("status " + status);*/
+    
+    // Validate components
+    if (typeof(requester) == undefined) throw "No requester sent"
+    if (typeof(requestee) == undefined) throw "No requestee sent"
+    if (typeof(app_id) == undefined) throw "No app_id sent"
 
     // Create a new submission
     let sql = `INSERT INTO submission (primary_student_email, app_id, room, sub_status)
-    VALUES ("${requester}", "${app_id}", "${room}", "${status}")`;
+        VALUES ("${requester}", "${app_id}", "${room}", "${status}")`;
     connection.query(sql, function (error, results, fields) {
         if (error) throw error;
         let newSubID = results.insertId;
@@ -137,3 +132,62 @@ app.post("/submission", function(req,res){
     //res.send(req.body);
     //console.log(req.body);
 })
+
+function determineAppType(app_id, callback){
+   
+    // Validate components
+    if (typeof(app_id) == undefined) throw "No app_id sent"
+
+    let sql = "Select * from application where app_id = " + app_id;
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        console.log('The application is: ', JSON.stringify(results));
+        console.log(results.app_type)
+        callback(results.app_type);
+    });
+
+}
+
+
+
+function traditionalSubmission(requester, requestee, app_id, res) {
+    // Create a new submission
+    let sql = `INSERT INTO submission (primary_student_email, app_id, sub_status)
+        VALUES ("${requester}", "${app_id}", "${status}")`;
+
+    connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        let newSubID = results.insertId;
+
+        // Create a new request
+        let sql2 = `INSERT INTO request (requester_email, requestee_email, submission_id, request_status)
+            VALUES ("${requester}", "${requestee}", ${newSubID}, "pending");`;
+
+        connection.query(sql2, function (error, results, fields) {
+            if (error) throw error;
+            let newReqID = results.insertId;
+            let sql3 = `INSERT INTO submissions_requests VALUES (${newSubID},${newReqID});`;
+
+            // Create a new submision/requests
+            connection.query(sql3, function (error, results, fields) {
+                console.log("new sub_request " + JSON.stringify(results));
+                res.send("Success...?");
+            });
+        });
+    });
+}
+
+
+app.post("/submission2", function(req,res){
+    let requester = connection.escape(req.body.requester);
+    let requestee = connection.escape(req.body.requestee);
+    let app_id = connection.escape(req.body.app_id);
+    determineAppType(app_id, function(app_type){
+        if (app_type == 'squatting') console.log("Do squat");
+        if (app_type == 'omega') console.log("Do omega");
+        if (app_type == 'nu') console.log("Do nu");
+        if (app_type == 'themed') console.log("Do theme");
+        if (app_type == 'traditional') console.log("Do tradition");
+        if (app_type == 'off-campus') console.log("Do off-campus");
+    });
+});
