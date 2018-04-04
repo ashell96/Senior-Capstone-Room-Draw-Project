2b5ec427a43317a42b739713a96d67e861d2d143
apellis@eckerd.edu <ashleyellis@Ashleys-MacBook-Pro-2.local>
2018-04-04 16:15:59 -0400

    add view requests component

---
+++ frontend/src/components/ViewRequests.vue
@@ -0,0 +1,36 @@
+<template>
+<span>
+<h1>Viewing Rommate Request </h1>
+<p> You have received a roommate request from ''</p>
+<p>Please enter your e-mail: 
+<input v-model="email" placeholder="Enter e-mail"> </p>
+
+
+
+<button class="btn btn-info btn-md">Click to Accept</button>
+<br>
+<br>
+<button class="btn btn-info btn-md">Click to Deny</button>
+
+</span>
+
+</template>
+
+<script>
+let axios = require("axios");
+module.exports = {
+  data: function() {
+    return { message: ""
+    };
+  },
+  methods: {
+    fun: function() {
+      console.log("fun");
+    },
+    sendtoApps : function(){
+      window.location = '/#/ViewSubmissions'
+      
+    }
+  }
+};
+</script>
