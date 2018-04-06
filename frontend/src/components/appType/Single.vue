833e223239f261c9d77019e24ad909a48e7809ce
apellis@eckerd.edu <ashleyellis@Ashleys-MacBook-Pro-2.local>
2018-04-06 01:01:10 -0400

    update single component

---
+++ frontend/src/components/appType/Single.vue
@@ -0,0 +1,38 @@
+<template>
+<span>
+<h1>Single Application </h1>
+<h5>For More Information Please Visit: www.eckerd.edu/housing</h5>
+<h5>Please complete this application and submit it no later than 12 Noon Monday, March 20, 2018</h5>
+<h5><u>Late Applications WILL NOT be accepted.</u></h5>
+<p><b>After Room Draw, if one member withdraws from the unit, the remaining members will be given the option 
+    to fill the space with another person of equal or greater point value within five business days. If after 
+    five business days the vacant space has not been filled, the remaining students <u>will lose</u> the right to 
+    occupy the unit. When two or more persons withdraw, at any time, from the unit the remaining members <u>will lose</u> the right to occupy the unit.</b></p>
+
+<p>Please enter your e-mail: 
+<input v-model="email" placeholder="Enter e-mail"> </p>
+
+
+
+<button v-on:click="sendtoApps()" class="btn btn-info btn-sm">Submit</button>
+</span>
+
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return { message: "Application", email:"", email2:"", email3:"", email4:"", email5:"", email6:"", email7:""
+    };
+  },
+  methods: {
+    fun: function() {
+      console.log("fun");
+    },
+    sendtoApps : function(){
+      window.location = '/#/ViewSubmissions'
+      
+    }
+  }
+};
+</script>
