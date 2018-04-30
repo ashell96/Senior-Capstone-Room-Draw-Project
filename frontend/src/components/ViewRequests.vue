f344edc1fcb6e4fa5c7d313905b714df4a605da1
apellis@eckerd.edu <ashleyellis@Ashleys-MacBook-Pro-2.local>
2018-04-30 12:36:35 -0400

    update component to read submission ID

--- frontend/src/components/ViewRequests.vue
+++ frontend/src/components/ViewRequests.vue
@@ -2,18 +2,15 @@
 <span>
 <h1>Viewing Rommate Request </h1>
 <p> You have received a roommate request from {{requester_email}}</p>
-<p>Room Request: {{submission_id}} (Submission ID for now to use in v-for loop) </p>
+<p>Room Request Submission ID: {{submission_id}}  </p>
 <button class="btn btn-info btn-md">Click to Accept</button>
 <br>
 <br>
 <button class="btn btn-info btn-md">Click to Deny</button>
                 <div id="hi" v-for="item in submissions" :key="item.submission_id">
-                   <td id="sub_id">{{item.submission_id}}</td>
-                    <td id="stu_email">{{item.primary_student_email}} </td>
-                    <td id="sub_date">{{new Date(item.submission_date).toLocaleString()}}</td>
-                    <td id="app_id">{{item.app_id}}</td>
-                    <td id="room">{{item.room}}</td>
-                    <td id="sub_status">{{item.sub_status}}</td>
+                    <td v-if="item.submission_id == submission_id" id="room"> Room: {{item.room}}</td>
+                    <br>
+                    <td v-if="item.submission_id == submission_id" id="sub_status"> Status: {{item.sub_status}}</td>
                   
                 </div>
 </span>
@@ -39,7 +36,7 @@ module.exports = {
         setTimeout(function(){vm.loadApplications()},1000);
       } else {
         axios
-              .post("http://entropy7.nas.eckerd.edu:3000/mySubmissions/", {
+              .get("http://entropy7.nas.eckerd.edu:3000/mySubmissions/", {
                 myEmail: this.$props.curUserEmail
               })
               .then(function(response) {
